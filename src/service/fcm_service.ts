bdcdbf954f3e7f5fafc173c943c47417ee95b199
lotaviods <l.otavio9099@gmail.com>
2023-06-13 22:46:24 -0300

    fix: Crash when registration token not registered

--- src/service/fcm_service.ts
+++ src/service/fcm_service.ts
@@ -5,10 +5,10 @@ import admin from 'firebase-admin'
 
 dotenv.config()
 
-export async function sendMessage(message: Message): Promise<any>{
+export async function sendMessage(message: Message): Promise<any> {
     let fcmMessage: TokenMessage | TopicMessage | undefined = undefined
 
-    if(message.token_device != undefined)
+    if (message.token_device != undefined)
         fcmMessage = {
             "token": message.token_device,
             "notification": {
@@ -17,6 +17,16 @@ export async function sendMessage(message: Message): Promise<any>{
             }
         }
 
-    if(fcmMessage != undefined)
-        admin.messaging().send(fcmMessage);
+    if (fcmMessage == undefined) {
+        return
+    }
+    try {
+        await admin.messaging().send(fcmMessage);
+    } catch (error) {
+        if ((error as any).code === 'messaging/registration-token-not-registered') {
+            console.log('Registration token is not registered.');
+        } else {
+            throw error;
+        }
+    }
 }
